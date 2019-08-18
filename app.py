67a4b86a75083d1f02359b730b093e5da246a056
Babelac <babelacuniverse@gmail.com>
2019-08-19 00:48:25 +0700

    change reply msg

--- app.py
+++ app.py
@@ -45,21 +45,17 @@ def handle_text_message(event):
     incoming_msg = event.message.text
 
     print('Income message: ' + incoming_msg)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage('Yo whatsup'))
-
-    print('Testing..')
+    if incoming_msg == 'bales dong':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage('knp ey?'))
 
     msg_join = 'Congratulations!! You are joining the Werewolf Game'
     if incoming_msg == '/userid':
         if isinstance(event.source, SourceGroup):
             profile = line_bot_api.get_profile(event.source.user_id)
-            print('Profile: ' + profile)
             userid = profile.user_id
-            print(userid)
+            print('User ID: ' + userid)
             print('Start sending to ' + profile.display_name)
             line_bot_api.push_message(userid, TextSendMessage(msg_join))
-
-
 
 
 
