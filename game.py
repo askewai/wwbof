9f8dd24f51066215ac451613016a65fd7d0c0e3e
Babelac <babelacuniverse@gmail.com>
2019-08-21 00:29:40 +0700

    lele

--- game.py
+++ game.py
@@ -1,13 +1,42 @@
+import os
+from decouple import config
+from flask import (Flask, request, abort)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import InvalidSignatureError
+import json
+from linebot.models import (
+    MessageEvent, 
+    TextMessage, 
+    TextSendMessage,
+    SourceUser,
+    SourceGroup,
+    SourceRoom,
+    MessageAction,
+    LeaveEvent,
+    JoinEvent
+)
+import requests
+import time
 
 def main(): # 4 - 6 players
+    # DEFINE VARIABLES
     msg_join = 'Congratulations!! You are joining the Werewolf Game'
+
+
+    ###############################################
+
     if incoming_msg == '/join':
         if isinstance(event.source, SourceGroup):
             userid = []
             profile = line_bot_api.get_profile(event.source.user_id)
-            userid.append(profile.user_id)
-            print('User ID: ' + userid)
-            print('Start sending to ' + profile.display_name)
-            line_bot_api.push_message(userid, TextSendMessage(msg_join))
+            for x in range(len(userid)): # 0 - 2
+                if profile.user_id != userid[x]:
+                    userid.append(profile.user_id) 
+                    print('User ID: ' + userid)
+                    print('Start sending to ' + profile.display_name)
+                    line_bot_api.push_message(userid, TextSendMessage(msg_join))
+            
+            print('Users that join the game ' + str(userid))
             
             if incoming_msg == '/startgame':
+                
