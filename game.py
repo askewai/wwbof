9754400b27892a522f5ad5a50b9e7394581901fb
Babelac <babelacuniverse@gmail.com>
2019-08-21 00:58:21 +0700

    dumbo

--- game.py
+++ game.py
@@ -17,26 +17,53 @@ from linebot.models import (
 )
 import requests
 import time
+import re
 
 def main(): # 4 - 6 players
     # DEFINE VARIABLES
     msg_join = 'Congratulations!! You are joining the Werewolf Game'
-
+    str_curr = 'Current players: \n'
+    player = []
 
     ###############################################
 
-    if incoming_msg == '/join':
-        if isinstance(event.source, SourceGroup):
+    if incoming_msg == '/join': # If user type '/join'
+        if isinstance(event.source, SourceGroup): # If eventnya dari group
             userid = []
             profile = line_bot_api.get_profile(event.source.user_id)
-            for x in range(len(userid)): # 0 - 2
-                if profile.user_id != userid[x]:
-                    userid.append(profile.user_id) 
-                    print('User ID: ' + userid)
-                    print('Start sending to ' + profile.display_name)
-                    line_bot_api.push_message(userid, TextSendMessage(msg_join))
+            if len(userid) >= 4 and len(userid) <= 6: # If total players antara 4-6
+                for x in range(len(userid)): 
+                    if profile.user_id != userid[x]: # If ada player baru
+                        userid.append(profile.user_id) 
+                        print('User ID: ' + userid)
+                        line_bot_api.push_message(userid, TextSendMessage(msg_join))
+                        for y in range(len(userid)):
+                            player.append(str(y+1) + '. ' + profile.display_name)
+
+                        players = '\n'.join(player)
+                        line_bot_api.reply_message(event.reply_token, TextSendMessage(str_curr + players))
+                            
+                print('PLAYERS: ' + str(userid))
+
+                
+                if incoming_msg == '/startgame':
+                    
+            elif len(userid) >= 7 and len(userid) <= 12:
+
+
+
+
+            elif len(userid) < 4:   # If players kurang dari 4
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you are too lonely (min 4 ppl)')
+
+            elif len(userid) > 12:  # If players lebih dari 12
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you guys are too crowded (max 12 ppl)')
             
-            print('Users that join the game ' + str(userid))
-            
-            if incoming_msg == '/startgame':
+            else:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Werewolf is under maintenance :)')
+
+
+
+    if incoming_msg == '/quit':
+
                 
