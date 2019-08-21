560bdd6a8b17810efec5ddac22a28ded7206ea83
Babelac <babelacuniverse@gmail.com>
2019-08-21 21:54:25 +0700

    bugging

--- game.py
+++ game.py
@@ -31,6 +31,7 @@ def main(): # 4 - 6 players
         if isinstance(event.source, SourceGroup): # If eventnya dari group
             userid = []
             profile = line_bot_api.get_profile(event.source.user_id)
+
             if len(userid) >= 4 and len(userid) <= 6: # If total players antara 4-6
                 for x in range(len(userid)): 
                     if profile.user_id != userid[x]: # If ada player baru
@@ -48,19 +49,19 @@ def main(): # 4 - 6 players
                 
                 if incoming_msg == '/startgame':
                     print('start game di click')
-            elif len(userid) >= 7 and len(userid) <= 12:
+            elif len(userid) >= 7 and len(userid) <= 12: # If total players antara 7-12
                 print('antara 7-12 players')
 
 
 
             elif len(userid) < 4:   # If players kurang dari 4
-                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you are too lonely (min 4 ppl)')
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you are too lonely (min 4 ppl)'))
 
             elif len(userid) > 12:  # If players lebih dari 12
-                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you guys are too crowded (max 12 ppl)')
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you guys are too crowded (max 12 ppl)'))
             
             else:
-                line_bot_api.reply_message(event.reply_token, TextSendMessage('Werewolf is under maintenance :)')
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Werewolf is under maintenance :)'))
 
 
 
