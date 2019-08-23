418e18ffe678435bb3946d9f3c0ae8355de0474e
Babelac <babelacuniverse@gmail.com>
2019-08-24 00:07:52 +0700

    update

--- game.py
+++ game.py
@@ -78,56 +78,57 @@ def main(event, line_bot_api, handler, incoming_msg):
     #########################################################
     
     if incoming_msg == '/startgame' and state == 0: # Kasih role ke masing" orang, randomize depends on the num of players
-        
-        if len(userid) >= 4 and len(userid) <= 6: # If total players antara 4-6
-            state = 1
-            print('Game has started | 4-6 players')
-            line_bot_api.reply_message(event.reply_token, TextSendMessage('The game has started!! \nAuuuuuuuwwww!! Who is the werewolf here? Let\'s find out!'))
-        
-            #################################################################### 
-
-            role = ['Werewolf', 'Seer']
-            for y in range(len(userid)-2):
-                role.append('Villager')
-            random.shuffle(role) # Randomize the roles
-
-            data = {
-                'player': [
-                ]
-            }
-
-            # Assign to dictionaries
-            for x in range(len(userid)): # 0 - 3
-                each_data = {
-                    "userid": userid[x],
-                    "displayname": displayname[x],
-                    "role": role[x]
-                }
-                data['player'].append(each_data)
+        if isinstance(event.source, SourceGroup):
+            if len(userid) >= 4 and len(userid) <= 6: # If total players antara 4-6
+                state = 1
+                print('Game has started | 4-6 players')
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('The game has started!! \nAuuuuuuuwwww!! Who is the werewolf here? Let\'s find out!'))
             
-            print('UserID: ' + str(userid))
-            print('Display name: ' + str(displayname))
-            print('Role: ' + str(role))
-            print('DATA: ' + str(data))
-            print(data['player'][0]['displayname'])
-            print(data['player'][3]['userid'])
+                #################################################################### 
 
-        
-        elif len(userid) >= 7 and len(userid) <= 12: # If total players antara 7-12
-            state = 1
-            print('Game has started | 7-12 players')
-            line_bot_api.reply_message(event.reply_token, TextSendMessage('The game has started!! \nAuuuuuuuwwww!! Who are the werewolves here? Let\'s find out!'))
-        elif len(userid) < 4: # If players kurang dari 4
-            line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you are too lonely (min 4 ppl)'))
-        elif len(userid) > 12:  # If players lebih dari 12
-            line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you guys are too crowded (max 12 ppl)'))
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage('Werewolf is under maintenance :)'))
+                # Define data
+                data = {'player': []}
+
+                # Randomize roles for 4-6 players
+                role = ['Werewolf', 'Seer']
+                for y in range(len(userid)-2):
+                    role.append('Villager')
+                random.shuffle(role) 
+
+                # Assign to dictionaries
+                for x in range(len(userid)): # 0 - 3
+                    each_data = {
+                        "userid": userid[x],
+                        "displayname": displayname[x],
+                        "role": role[x]
+                    }
+                    data['player'].append(each_data)
+                    line_bot_api.push_message(data['player'][x]['userid'], TextSendMessage('Your role is:' + data['player'][x]['role']))
+
+                print('UserID: ' + str(userid))
+                print('Display name: ' + str(displayname))
+                print('Role: ' + str(role))
+                print('DATA: ' + str(data))
+                # print(data['player'][0]['displayname'])
+                # print(data['player'][3]['userid'])
+
+                
+
+
+            
+            elif len(userid) >= 7 and len(userid) <= 12: # If total players antara 7-12
+                state = 1
+                print('Game has started | 7-12 players')
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('The game has started!! \nAuuuuuuuwwww!! Who are the werewolves here? Let\'s find out!'))
+            elif len(userid) < 4: # If players kurang dari 4
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you are too lonely (min 4 ppl)'))
+            elif len(userid) > 12:  # If players lebih dari 12
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Sorry, you guys are too crowded (max 12 ppl)'))
+            else:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage('Werewolf is under maintenance :)'))
 
     print('STATE: ' + str(state))
 
     # if incoming_msg == '/leave':
     #     line_bot_api.reply_message(event.reply_token, TextSendMessage('See you next game, '))
     
-
-                
