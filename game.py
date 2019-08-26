a7c521b1fa8d5477781ba7746e67722d46dce271
Kevin Kurnia <kevin.kurnia@id.corp.seagroup.com>
2019-08-26 10:26:11 +0700

    update

--- game.py
+++ game.py
@@ -158,7 +158,7 @@ def main(event, line_bot_api, handler, incoming_msg):
                     # check if werewolf == 0 -> end game (manggil function game over)
 
                     # For testing only
-                    if incoming_msg == 'stop':
+                    if incomie_msg == 'stop':
                         break
 
 
