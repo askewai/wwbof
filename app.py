2bbcd5c0201e5c627b76726d1926f3a50c2c7a0e
Mayesq Prameswari <mayesqprm@gmail.com>
2025-05-10 20:41:01 +0700

    c

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ import requests
 import game
 
 app = Flask(__name__)
+
 # get LINE_CHANNEL_ACCESS_TOKEN from your environment variable
 line_bot_api = LineBotApi(config("LINE_CHANNEL_ACCESS_TOKEN", default=os.environ.get('LINE_ACCESS_TOKEN')))
 # get LINE_CHANNEL_SECRET from your environment variable
@@ -26,18 +27,20 @@ handler = WebhookHandler(config("LINE_CHANNEL_SECRET", default=os.environ.get('L
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # Ambil signature dari header request
     signature = request.headers.get('X-Line-Signature')
 
-    # get request body as text                                  
+    # Ambil body dari request sebagai text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # Menangani webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
+
 
 @app.route("/")
 def home():
@@ -45,7 +48,7 @@ def home():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    incoming_msg = (event.message.text).lower()           
+    incoming_msg = (event.message.text).lower()
 
     print('Income message: ' + incoming_msg)
     
@@ -64,10 +67,11 @@ def handle_text_message(event):
     elif incoming_msg == 'bales dong':
         line_bot_api.reply_message(event.reply_token, TextSendMessage('knp ey?'))
 
-    # Call main game function         
+    # Call main game function
     game.main(event, line_bot_api, handler, incoming_msg)
 
 
 if __name__ == "__main__":
+    # Menggunakan port dari environment variable jika ada, jika tidak default ke 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
